468314220ae38da6dc3785f1e01ff2d3937341ef
leipq <1402936744@qq.com>
2019-04-03 17:53:06 +0800

    thread

---
+++ JavaDP/src/kingdee/base/ssc/day010/thread/base/SynchronizedFactorizer.java
@@ -0,0 +1,33 @@
+package kingdee.base.ssc.day010.thread.base;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.math.BigInteger;
+
+public class SynchronizedFactorizer implements Servlet {
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
